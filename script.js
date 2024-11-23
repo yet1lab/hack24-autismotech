04acf02a33a88a75b2d726d455114a66091bad89
victor yghor <victor.yghor@ufrpe.br>
2024-11-23 13:33:36 -0300

    feat: echence assebility

--- script.js
+++ script.js
@@ -1,18 +1,47 @@
 function openModal(modalId) {
+  console.log("open called here")
   document.body.style.overflow = 'hidden'; 
   const modal = document.getElementById(modalId);
+  
   if (modal) {
+    // Exibe o modal
     modal.style.display = 'flex';
+    
+    // Define os atributos de acessibilidade
+    modal.setAttribute('aria-hidden', 'false');
+    modal.setAttribute('aria-modal', 'true');
+    modal.setAttribute('role', 'dialog');
+
+    // Esconde o conteúdo de fundo
+    document.querySelectorAll('body > *:not(dialog)').forEach(element => {
+      console.log(element)
+      element.setAttribute('aria-hidden', 'true');
+    });
+    console.log("modal " + modalId)
+    modal.focus()
   }
 }
+
 
 function closeModal(modalId) {
   document.body.style.overflow = '';
   const modal = document.getElementById(modalId);
+  
   if (modal) {
+    // Esconde o modal
     modal.style.display = 'none';
+    
+    // Atualiza os atributos de acessibilidade
+    modal.setAttribute('aria-hidden', 'true');
+    modal.removeAttribute('aria-modal');
+    
+    // Mostra o conteúdo de fundo
+    document.querySelectorAll('body > *:not(dialog)').forEach(element => {
+      element.removeAttribute('aria-hidden');
+    });
   }
 }
+
 
 function closeAnyModal() {
   document.body.style.overflow = '';  
@@ -25,12 +54,14 @@ function closeAnyModal() {
 function activateTranslate() {
   closeModal('modal1');
   openModal('modal2');
+  const aceitarModal = document.querySelect("modal2")
+  aceitarModal.focus()
 }
 
 function feedbackTranslate() {
   closeModal('modal2');
   alert('Traduz ativado com sucesso!');
-
+  window.location.href = "correct.html";
   document.getElementById('translate-text').style.display = 'inline'; 
 }
 
